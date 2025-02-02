905390b152f5346776d6a1d47c2647c20aff0a8c
deniselksina <elksina40@gmail.com>
2025-02-02 20:44:51 +0300

    Create main.h

---
+++ include/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "App.h"
+#include "Browser.h"
+#include "Utils.h"
+#include "Network.h"
+#include "GUI.h"
+#include "WebView.h"
+
+// Объявление функции main
+int main();
+
+#endif
